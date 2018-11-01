b04e7a10611de40178d7551d9af50326f2d5fe1b
James Carlson <jxxcarlson@mac.com>
2018-10-31 20:21:02 -0400

    Add module tests/Strings.elm

--- tests/Example.elm
+++ tests/Example.elm
@@ -6,6 +6,7 @@ import Test exposing (..)
 import Network exposing(..)
 import Decoder exposing(..)
 import Json.Decode exposing(decodeString)
+import Strings exposing(..)
 
 doTest comment inputExpression outputExpression =
     test comment <|
@@ -22,55 +23,7 @@ doFloatTest comment inputExpression outputExpression =
 net = buildNetwork [u1, u2, u3, u4] [e14, e12, e43, e23 ] 
 
 
-netAsJson = """
-  {
-    "nodes": [
-      {"name": "U1" }, 
-      {"name": "U2" },
-      {"name": "U3" }, 
-      {"name": "U4" }
-    ], 
-    "edges": [
-        {
-          "initialNode": "U1", 
-          "terminalNode": "U4",
-          "flow": 30
-        },
-        {
-          "initialNode": "U1", 
-          "terminalNode": "U2",
-          "flow": 90.4
-        },
-        {
-          "initialNode": "U4", 
-          "terminalNode": "U3",
-          "flow": 22
-        },
-        {
-          "initialNode": "U2", 
-          "terminalNode": "U3",
-          "flow": 31.4
-        }
-    ]
-  }
 
-"""
-
-nodeAAsJson = """
-  {"name": "A" }
-"""
-
-nodeBAsJson = """
-  {"name": "B"}
-"""
-
-edgeABAsJson = """
-  {
-     "initialNode": {"name": "A" }, 
-     "terminalNode": {"name": "B" },
-     "flow": 17.3
-  }
-"""
 
 suite : Test
 suite =
@@ -81,40 +34,40 @@ suite =
     ]
 
 creationSuite = describe "Create nodes, edges, networks" 
-            [ 
-              doTest
-                "1. Create node"
-                (name (createNode "A"))
-                ("A")
-            , doTest
-                "2. Equal nodes"
-                (equalNodes (createNode "A") (createNode "A"))
-                (True)
-            , doTest
-                "2. Unequal nodes"
-                (equalNodes (createNode "A") (createNode "B"))
-                (False)
-            , doTest
-                "3. Build network and count nodes"
-                (nodeCount net)
-                (4)
-            , doTest
-                "4. Build network and count edges"
-                (edgeCount net)
-                (4)
-            , doTest
-                "5. insertEdge"
-                ((edgeCount (insertEdge "U1" "U3" 10 net)))
-                (5)
-             , doTest
-                "6. deleteEdge"
-                ((edgeCount (deleteEdge "U1" "U4" net)))
-                (3)
-            , doTest
-                "7. replaceEdge"
-                ((Maybe.map edgeFlow (getEdge "U1" "U4" (replaceEdge "U1" "U4" 1.234 net))))
-                (Just 1.234)
-        ] 
+    [ 
+        doTest
+        "1. Create node"
+        (name (createNode "A"))
+        ("A")
+    , doTest
+        "2. Equal nodes"
+        (equalNodes (createNode "A") (createNode "A"))
+        (True)
+    , doTest
+        "2. Unequal nodes"
+        (equalNodes (createNode "A") (createNode "B"))
+        (False)
+    , doTest
+        "3. Build network and count nodes"
+        (nodeCount net)
+        (4)
+    , doTest
+        "4. Build network and count edges"
+        (edgeCount net)
+        (4)
+    , doTest
+        "5. insertEdge"
+        ((edgeCount (insertEdge "U1" "U3" 10 net)))
+        (5)
+        , doTest
+        "6. deleteEdge"
+        ((edgeCount (deleteEdge "U1" "U4" net)))
+        (3)
+    , doTest
+        "7. replaceEdge"
+        ((Maybe.map edgeFlow (getEdge "U1" "U4" (replaceEdge "U1" "U4" 1.234 net))))
+        (Just 1.234)
+  ] 
 
 flowSuite = describe "Test flows" 
     [ 
@@ -143,9 +96,12 @@ flowSuite = describe "Test flows"
 jsonSuite = describe "Json decoders"
   [
     doTest
-      "1. decode Nodge"
-      (decodeString decodeNode nodeA)
+      "1. decode Node"
+      (decodeString decodeNode nodeAAsJson)
       (Ok (Node "A"))
-
+    , doTest
+      "2. decode Edge"
+      (decodeString decodeEdge edgeABAsJson)
+      (Ok (Edge (Node "A") (Node "B") 17.3))
 
   ]
