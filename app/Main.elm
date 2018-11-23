c735697b9bbb507667073e5b50fb106795e9031d
James Carlson <jxxcarlson@mac.com>
2018-11-23 11:16:28 -0500

    Enable user to add, modify, and delete edges

--- app/Main.elm
+++ app/Main.elm
@@ -1,27 +1,28 @@
-module Main exposing(main)
+module Main exposing (main)
 
 import Browser
-import Element exposing(..)
+import Element exposing (..)
 import Element.Font as Font
 import Element.Input as Input
-import Html exposing(Html)
+import Html exposing (Html)
 import Html.Attributes
-import Network exposing(
-       Network(..)
-     , emptyNetwork 
-    )
-import NetworkParser exposing(networkFromString)
+import Network
+    exposing
+        ( Network(..)
+        , emptyNetwork
+        )
+import NetworkParser exposing (networkFromString)
 import Widget
 import Svg exposing (svg)
 import Display
-
-import FlowModel exposing(
-    exampleNetwork
-    , displayListWithTitle
-    , displayNodes
-    , displayEdges
-    , report
-  )
+import FlowModel
+    exposing
+        ( exampleNetwork
+        , displayListWithTitle
+        , displayNodes
+        , displayEdges
+        , report
+        )
 
 
 main =
@@ -32,103 +33,187 @@ main =
         , subscriptions = subscriptions
         }
 
-type Msg = 
-  NoOp
-  | InputNetworkString String
-  | UpdateNetwork 
 
-type alias Model = { 
-    message: String
+type Msg
+    = NoOp
+    | InputNetworkString String
+    | InputSourceNode String
+    | InputTargetNode String
+    | InputEdgeFlow String
+    | UpdateNetwork
+
+
+type alias Model =
+    { message : String
     , networkAsString : String
-    , network : Network }
+    , network : Network
+    , sourceNode : String
+    , targetNode : String
+    , edgeFlow : String
+    }
 
-initialNetworkString = """U1, U4, 30; U1, U2, 90.4; U4, U3, 22; U2, U3, 31.4;
+
+initialNetworkString =
+    """U1, U4, 30; U1, U2, 90.4; U4, U3, 22; U2, U3, 31.4;
 V1, V4, 30; V1, V2, 90.4; V4, V3, 22; V2, V3, 31.4;
 U1, V1, 30;
 """
 
-initialModel : Model  
-initialModel = 
-  { message = "Hello!"
-  , networkAsString =  initialNetworkString
-  , network =  networkFromString initialNetworkString
 
- }
+initialModel : Model
+initialModel =
+    { message = "Hello!"
+    , networkAsString = initialNetworkString
+    , network = networkFromString initialNetworkString
+    , sourceNode = ""
+    , targetNode = ""
+    , edgeFlow = ""
+    }
+
 
 type alias Flags =
     {}
 
+
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( initialModel, Cmd.none)
+    ( initialModel, Cmd.none )
 
 
 subscriptions model =
     Sub.batch
-        [  ]
+        []
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            ( model, Cmd.none ) 
+            ( model, Cmd.none )
+
         InputNetworkString str ->
-            ({model | networkAsString = str }, Cmd.none)
+            ( { model | networkAsString = str }, Cmd.none )
+
         UpdateNetwork ->
-          ({model | network =  networkFromString model.networkAsString}, Cmd.none )
+            let
+                newEdgeFlow =
+                    String.toFloat model.edgeFlow |> Maybe.withDefault 0
+
+                newNetworkAsString =
+                    if model.sourceNode /= "" && model.targetNode /= "" && newEdgeFlow > 0 then
+                        model.networkAsString
+                            |> NetworkParser.simpleEdgeListFromString
+                            |> Network.replaceSimpleEdge model.sourceNode model.targetNode newEdgeFlow
+                            |> NetworkParser.stringOfSimpleEdgeList
+                    else if model.sourceNode /= "" && model.targetNode /= "" && newEdgeFlow == 0 then
+                        model.networkAsString
+                            |> NetworkParser.simpleEdgeListFromString
+                            |> Network.deleteSimpleEdge model.sourceNode model.targetNode
+                            |> NetworkParser.stringOfSimpleEdgeList
+                    else
+                        model.networkAsString
+            in
+                ( { model
+                    | networkAsString = newNetworkAsString
+                    , network = networkFromString newNetworkAsString
+                  }
+                , Cmd.none
+                )
+
+        InputSourceNode str ->
+            ( { model | sourceNode = str }, Cmd.none )
+
+        InputTargetNode str ->
+            ( { model | targetNode = str }, Cmd.none )
+
+        InputEdgeFlow str ->
+            ( { model | edgeFlow = str }, Cmd.none )
 
 
 view : Model -> Html Msg
-view model = 
-  layout [] (mainRow model)
+view model =
+    layout [] (mainRow model)
+
 
 mainRow : Model -> Element Msg
 mainRow model =
-  column [width fill, centerX, centerY, spacing 40, Font.size 16 ] [
-    el [Font.bold, Font.size 24, centerX] (text "Network")
-    , row [centerX, spacing 60 ] (networkDisplay model)
-    , row [centerX] [networkInput model]
-    , row [centerX] [updateNetworkButton model]
-  ]
- 
-      
-
-networkDisplay : Model -> List (Element msg)
-networkDisplay model = 
-     [
-           column  [centerX, alignTop] [displayNetwork model]
-         , column dataColumnStyle (displayListWithTitle "Nodes" <| displayNodes model.network)
-         , column dataColumnStyle (displayListWithTitle "Edges" <| displayEdges model.network)
-         , column [centerX, alignTop ] [report model.network]
-     ]
-
-dataColumnStyle = [centerX, spacing 10, alignTop, scrollbarX, height (px 300)]
+    column [ width fill, centerX, centerY, spacing 40, Font.size 16 ]
+        [ el [ Font.bold, Font.size 24, centerX ] (text "Network")
+        , row [ centerX, spacing 60 ] (networkDisplay model)
+        , row [ centerX ] [ networkEntryForm model ]
+        , row [ centerX ] [ networkInput model ]
+        , row [ centerX ] [ updateNetworkButton model ]
+        ]
 
 
-networkInput : Model -> Element Msg 
-networkInput model = 
-    Input.multiline [width (px 600), height (px 120) ]
-      {
-        onChange = InputNetworkString
-      , text = model.networkAsString 
-      , placeholder = Nothing
-      , label =   Input.labelAbove [ Font.size 18, Font.bold ] (text "Network data")
-      , spellcheck = False
+networkDisplay : Model -> List (Element Msg)
+networkDisplay model =
+    [ column [ centerX, alignTop ] [ displayNetwork model ]
+    , column dataColumnStyle (displayListWithTitle "Nodes" <| displayNodes model.network)
+    , column dataColumnStyle (displayListWithTitle "Edges" <| displayEdges model.network)
+    , column [ centerX, alignTop ] [ report model.network ]
+    ]
 
-      }
 
-updateNetworkButton : Model -> Element Msg    
-updateNetworkButton model = 
-  Input.button Widget.buttonStyle {
-    onPress =  Just UpdateNetwork
-  , label = Element.text "Update Network"
-  } 
+dataColumnStyle =
+    [ centerX, spacing 10, alignTop, scrollbarX, height (px 300) ]
+
+
+networkInput : Model -> Element Msg
+networkInput model =
+    Input.multiline [ width (px 600), height (px 120) ]
+        { onChange = InputNetworkString
+        , text = model.networkAsString
+        , placeholder = Nothing
+        , label = Input.labelAbove [ Font.size 18, Font.bold ] (text "Network data")
+        , spellcheck = False
+        }
+
+
+updateNetworkButton : Model -> Element Msg
+updateNetworkButton model =
+    Input.button Widget.buttonStyle
+        { onPress = Just UpdateNetwork
+        , label = Element.text "Update Network"
+        }
 
 
 displayNetwork : Model -> Element msg
 displayNetwork model =
-    Element.html 
-      (svg [ Html.Attributes.width 400, Html.Attributes.height 400 ] (Display.networkDisplay 200 model.network))
+    Element.html
+        (svg [ Html.Attributes.width 400, Html.Attributes.height 400 ]
+            (Display.networkDisplay 200 model.network)
+        )
 
-    
 
+networkEntryForm : Model -> Element Msg
+networkEntryForm model =
+    row [ spacing 8 ]
+        [ inputSourceNode model, inputTargetNode model, inputEdgeFlow model ]
+
+
+inputSourceNode model =
+    Input.text [ height (px 25), width (px 100) ]
+        { onChange = InputSourceNode
+        , text = model.sourceNode
+        , placeholder = Nothing
+        , label = Input.labelAbove [ Font.size 14, Font.bold ] (text "Source node")
+        }
+
+
+inputTargetNode model =
+    Input.text [ height (px 25), width (px 100) ]
+        { onChange = InputTargetNode
+        , text = model.targetNode
+        , placeholder = Nothing
+        , label = Input.labelAbove [ Font.size 14, Font.bold ] (text "Target node")
+        }
+
+
+inputEdgeFlow model =
+    Input.text [ height (px 25), width (px 100) ]
+        { onChange = InputEdgeFlow
+        , text = model.edgeFlow
+        , placeholder = Nothing
+        , label = Input.labelAbove [ Font.size 14, Font.bold ] (text "Flow")
+        }
--- src/NetworkParser.elm
+++ src/NetworkParser.elm
@@ -1,56 +1,69 @@
-module NetworkParser exposing(..)
+module NetworkParser exposing (..)
 
-import Parser exposing(..)
-
-import Network exposing(
-      Network(..)
-      , Node
-      , SimpleEdge(..)
-      , Edge(..)
-      , createNode
-      , sourceNameOfSimpleEdge
-      , sinkNameOfSimpleEdge)
-
-import Tools exposing(unique)
+import Parser exposing (..)
+import Network
+    exposing
+        ( Network(..)
+        , Node
+        , SimpleEdge(..)
+        , Edge(..)
+        , createNode
+        , sourceNameOfSimpleEdge
+        , sinkNameOfSimpleEdge
+        )
+import Tools exposing (unique)
 
 
 simpleEdgeListParser : Parser (List SimpleEdge)
-simpleEdgeListParser = 
-  many simpleEdgeParser
+simpleEdgeListParser =
+    many simpleEdgeParser
 
-simpleEdgeParser : Parser SimpleEdge 
-simpleEdgeParser = 
-  succeed SimpleEdge  
-    |= identifier 
-    |. symbol ","
-    |. spaces  
-    |= identifier 
-    |. symbol ","
-    |. spaces   
-    |= float  
-    |. symbol ";"
-    |. spaces    
+
+stringOfSimpleEdgeList : List SimpleEdge -> String
+stringOfSimpleEdgeList edgeList =
+    edgeList
+        |> List.map Network.stringOfSimpleEdge
+        |> String.join " "
+
+
+simpleEdgeParser : Parser SimpleEdge
+simpleEdgeParser =
+    succeed SimpleEdge
+        |= identifier
+        |. symbol ","
+        |. spaces
+        |= identifier
+        |. symbol ","
+        |. spaces
+        |= float
+        |. symbol ";"
+        |. spaces
+
 
 identifier : Parser String
 identifier =
-  getChompedString <|
-    succeed ()
-      |. chompIf isStartChar
-      |. chompWhile isInnerChar
+    getChompedString <|
+        succeed ()
+            |. chompIf isStartChar
+            |. chompWhile isInnerChar
+
 
 isStartChar : Char -> Bool
 isStartChar char =
-  Char.isAlpha char
+    Char.isAlpha char
+
 
 isInnerChar : Char -> Bool
 isInnerChar char =
-  isStartChar char || Char.isDigit char
+    isStartChar char || Char.isDigit char
+
 
 {-| Apply a parser zero or more times and return a list of the results.
 -}
 many : Parser a -> Parser (List a)
 many p =
     loop [] (manyHelp p)
+
 
 manyHelp : Parser a -> List a -> Parser (Step (List a) (List a))
 manyHelp p vs =
@@ -63,59 +76,43 @@ manyHelp p vs =
         ]
 
 
-
-
-
-networkFromString : String -> Network   
+networkFromString : String -> Network
 networkFromString str =
-  Network (nodesFromString str) (edgeListFromString str)
+    Network (nodesFromString str) (edgeListFromString str)
 
 
-simpleEdgeListFromString : String -> (List SimpleEdge)
-simpleEdgeListFromString str = 
-  case run simpleEdgeListParser str of 
-    Err _ -> []
-    Ok edgeList -> edgeList
+simpleEdgeListFromString : String -> List SimpleEdge
+simpleEdgeListFromString str =
+    case run simpleEdgeListParser str of
+        Err _ ->
+            []
 
-edgeListFromString : String -> (List Edge)
-edgeListFromString str = 
-  simpleEdgeListFromString str 
-    |> Network.edgeListFromSimpleEdgeList
-
-nodeNamesFromString : String -> (List String)
-nodeNamesFromString str  =
-  let 
-    simpleEdgeList = simpleEdgeListFromString str
-    sourceNodeNames = simpleEdgeList |> List.map sourceNameOfSimpleEdge
-    sinkNodeNames = simpleEdgeList |> List.map sinkNameOfSimpleEdge
-  in   
-    sourceNodeNames ++ sinkNodeNames |> unique |> List.sort
-
-nodesFromString : String -> List Node   
-nodesFromString str = 
-  nodeNamesFromString str
-    |> List.map createNode
+        Ok edgeList ->
+            edgeList
 
 
--- unique : List comparable -> List comparable
--- unique list =
---     uniqueHelp identity Set.empty list []
+edgeListFromString : String -> List Edge
+edgeListFromString str =
+    simpleEdgeListFromString str
+        |> Network.edgeListFromSimpleEdgeList
 
 
--- uniqueHelp : (a -> comparable) -> Set comparable -> List a -> List a -> List a
--- uniqueHelp f existing remaining accumulator =
---     case remaining of
---         [] ->
---             List.reverse accumulator
+nodeNamesFromString : String -> List String
+nodeNamesFromString str =
+    let
+        simpleEdgeList =
+            simpleEdgeListFromString str
 
---         first :: rest ->
---             let
---                 computedFirst =
---                     f first
---             in
---             if Set.member computedFirst existing then
---                 uniqueHelp f existing rest accumulator
+        sourceNodeNames =
+            simpleEdgeList |> List.map sourceNameOfSimpleEdge
 
---             else
---                 uniqueHelp f (Set.insert computedFirst existing) rest (first :: accumulator)
+        sinkNodeNames =
+            simpleEdgeList |> List.map sinkNameOfSimpleEdge
+    in
+        sourceNodeNames ++ sinkNodeNames |> unique |> List.sort
 
+
+nodesFromString : String -> List Node
+nodesFromString str =
+    nodeNamesFromString str
+        |> List.map createNode
