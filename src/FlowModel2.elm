c5f23eab1693d48fcfd64bcb1ec74eb8321a5c7b
James Carlson <jxxcarlson@mac.com>
2018-12-18 21:37:27 -0500

    All the functions in module Flow are now duplicated in module FlowModel2, where SimpleGraph is the base type

--- src/FlowModel2.elm
+++ src/FlowModel2.elm
@@ -1,6 +1,64 @@
-module FlowModel2 exposing (..)
+module FlowModel2 exposing (efficiency, resilience, alpha, sustainability, sustainabilityPercentage)
 
 import SimpleGraph as SG exposing (SimpleGraph, Edge)
+import Graph
+
+
+efficiency : SimpleGraph -> Float
+efficiency graph =
+    let
+        totalFlow_ =
+            SG.totalFlow graph
+
+        edges =
+            Graph.edges graph
+    in
+        List.map (efficiencyOfEdge totalFlow_ graph) edges
+            |> List.sum
+            |> roundTo 3
+
+
+resilience : SimpleGraph -> Float
+resilience graph =
+    let
+        totalFlow_ =
+            SG.totalFlow graph
+
+        edges =
+            Graph.edges graph
+    in
+        List.map (resilienceOfEdge totalFlow_ graph) edges
+            |> List.sum
+            |> \x -> -(roundTo 3 x)
+
+
+alpha : SimpleGraph -> Float
+alpha graph =
+    let
+        ratio =
+            1 + ((resilience graph) / (efficiency graph))
+    in
+        1 / ratio
+
+
+sustainability : SimpleGraph -> Float
+sustainability graph =
+    let
+        a =
+            alpha graph
+
+        aa =
+            a ^ 1.288
+
+        s =
+            -1.844 * aa * (logBase 2 aa)
+    in
+        roundTo 4 s
+
+
+sustainabilityPercentage : SimpleGraph -> Float
+sustainabilityPercentage graph =
+    roundTo 2 (100 * (sustainability graph))
 
 
 efficiencyOfEdge : Float -> SimpleGraph -> Edge -> Float
